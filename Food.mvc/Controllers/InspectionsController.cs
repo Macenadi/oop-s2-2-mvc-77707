ae2f45ce7b9479d2e342859de909c8e0d8bed84b
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-03-24 23:38:34 +0000

    Creating Controllers and Update Views-> Shared -> Layout

---
+++ Food.mvc/Controllers/InspectionsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Food.domain.Models;
+using Food.mvc.Data;
+
+namespace Food.mvc.Controllers
+{
+    public class InspectionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InspectionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inspections
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Inspections.Include(i => i.Premise);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Inspections/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inspection = await _context.Inspections
+                .Include(i => i.Premise)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (inspection == null)
+            {
+                return NotFound();
+            }
+
+            return View(inspection);
+        }
+
+        // GET: Inspections/Create
+        public IActionResult Create()
+        {
+            ViewData["PremiseId"] = new SelectList(_context.Premises, "Id", "Address");
+            return View();
+        }
+
+        // POST: Inspections/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PremiseId,InspectionDate,Score,Outcome,Notes")] Inspection inspection)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(inspection);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PremiseId"] = new SelectList(_context.Premises, "Id", "Address", inspection.PremiseId);
+            return View(inspection);
+        }
+
+        // GET: Inspections/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inspection = await _context.Inspections.FindAsync(id);
+            if (inspection == null)
+            {
+                return NotFound();
+            }
+            ViewData["PremiseId"] = new SelectList(_context.Premises, "Id", "Address", inspection.PremiseId);
+            return View(inspection);
+        }
+
+        // POST: Inspections/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PremiseId,InspectionDate,Score,Outcome,Notes")] Inspection inspection)
+        {
+            if (id != inspection.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inspection);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InspectionExists(inspection.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PremiseId"] = new SelectList(_context.Premises, "Id", "Address", inspection.PremiseId);
+            return View(inspection);
+        }
+
+        // GET: Inspections/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inspection = await _context.Inspections
+                .Include(i => i.Premise)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (inspection == null)
+            {
+                return NotFound();
+            }
+
+            return View(inspection);
+        }
+
+        // POST: Inspections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var inspection = await _context.Inspections.FindAsync(id);
+            if (inspection != null)
+            {
+                _context.Inspections.Remove(inspection);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InspectionExists(int id)
+        {
+            return _context.Inspections.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Food.mvc/Controllers/PremisesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Food.domain.Models;
+using Food.mvc.Data;
+
+namespace Food.mvc.Controllers
+{
+    public class PremisesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PremisesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Premises
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Premises.ToListAsync());
+        }
+
+        // GET: Premises/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var premise = await _context.Premises
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (premise == null)
+            {
+                return NotFound();
+            }
+
+            return View(premise);
+        }
+
+        // GET: Premises/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Premises/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Town,RiskRating")] Premise premise)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(premise);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(premise);
+        }
+
+        // GET: Premises/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var premise = await _context.Premises.FindAsync(id);
+            if (premise == null)
+            {
+                return NotFound();
+            }
+            return View(premise);
+        }
+
+        // POST: Premises/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Town,RiskRating")] Premise premise)
+        {
+            if (id != premise.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(premise);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PremiseExists(premise.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(premise);
+        }
+
+        // GET: Premises/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var premise = await _context.Premises
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (premise == null)
+            {
+                return NotFound();
+            }
+
+            return View(premise);
+        }
+
+        // POST: Premises/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var premise = await _context.Premises.FindAsync(id);
+            if (premise != null)
+            {
+                _context.Premises.Remove(premise);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PremiseExists(int id)
+        {
+            return _context.Premises.Any(e => e.Id == id);
+        }
+    }
+}
